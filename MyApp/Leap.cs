de2e00bf574ea1cfba5fbdd870c64feb814e6d83
nsel <nsel@itu.dk>
2022-09-04 13:07:54 +0200

    Created user interface for IsLeapYear.

--- MyApp/Leap.cs
+++ MyApp/Leap.cs
@@ -14,9 +14,33 @@ public class Leap
             return true;
         } else return false; 
     }
+    public bool isLeapYear(int year)
+    {
+        if(year%4 == 0 && (year%100 != 0 || year%400 == 0) && year!=0)
+        { 
+            setLeap();
+            return true;
+        } else return false; 
+    }
     private void setLeap()
     {
         isLeap=true;
+    }
+    public bool userInputCheck()
+    {
+
+        Console.Write("Enter year for Leapyear check!: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if(isLeapYear(number))
+        {
+            Console.Write("yay");
+            return true;
+        } else 
+        {
+            Console.WriteLine("nay");
+            return false;
+        }
     }
     
 }
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//uncomment to use leapYear console app 
+/**
+var leap = new Leap(5);
+leap.userInputCheck();
+*/
